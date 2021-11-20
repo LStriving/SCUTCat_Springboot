d1a0cccefe67119b81409976f8e79e4b7ac4b237
PSY <253480295@qq.com>
2021-11-20 23:09:06 +0800

    TestComment and TestSciArt were added

--- src/test/java/com/scutcat/demo/Mapper/TestComment.java
+++ src/test/java/com/scutcat/demo/Mapper/TestComment.java
@@ -12,11 +12,39 @@ public class TestComment {
     @Resource
     CommentMapper mapper;
     @Test
+    public void testGet(){
+        mapper.getComment("123");
+    }
+    @Test
     public void testAdd(){
-        mapper.addComment(new Comment("123","123","123456","这是一条评论"));
+        mapper.addComment(new Comment("120","123","123456","这又是一条评论"));
+    }
+    @Test
+    public void testDelete(){
+        mapper.deleteComment("123");
+    }
+    @Test
+    public void testUpdate(){
+        mapper.updateComment(new Comment("1234","123","123456","为什么不更新"));
     }
     @Test
     public void testLike(){
-        mapper.likeComment(new LikeComment("123456","123"));
+        mapper.likeComment(new LikeComment("123456","122"));
+    }
+    @Test
+    public void testLikeCommentUser(){
+        mapper.likeCommentUser("123");
+    }
+    @Test
+    public void testDislikeComment(){
+        mapper.dislikeComment(new LikeComment("123456","123"));
+    }
+    @Test
+    public void testGetCommentByPid(){
+        mapper.getCommentByPid("123");
+    }
+    @Test
+    public void testGetCommentByPidByTime(){
+        System.out.println(mapper.getCommentByPidByTime("123"));
     }
 }
